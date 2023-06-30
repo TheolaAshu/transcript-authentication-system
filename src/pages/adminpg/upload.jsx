15cd1f8e9e8144de3c7fcdf175232173e25e506f
TheolaAshu <ashutheola@gmail.com>
2023-06-30 16:46:13 -0700

    changed some texts

--- src/pages/adminpg/upload.jsx
+++ src/pages/adminpg/upload.jsx
@@ -18,7 +18,7 @@ function UploadPage() {
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="file">
-            Choose a transcript to upload:
+            Choose a transcript in pdf format to upload:
           </label>
           <input
             type="file"
