ba0b44a12adf5e2aebd7d55c5adf1b2c44be1609
Theola Ashu <ashutheola@gmail.com>
2023-07-15 07:34:45 +0100

    all pages

--- src/pages/adminpg/allTranscripts.jsx
+++ src/pages/adminpg/allTranscripts.jsx
@@ -1,25 +1,58 @@
+// import React from "react";
+// import { AiOutlineSearch } from "react-icons/ai";
+// import { AdminSidebar } from "../../components/admin-sidebar";
+
+// const AllTranscripts = () => {
+//   return (
+//     <div className="min-h-screen flex felx-col justify-center relative bg-gray-300">
+//       <div className="flex h-screen"><AdminSidebar/></div>
+//       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+//         <div className="flex justify-between">
+//           <h1 className="flex justify-center text-3xl font-bold text-black">
+//             All Transcripts
+//           </h1>
+//         </div>
+//         <div className="mt-6">
+//           <form className="flex items-center h-[60px] bg-red-500 flex-row">
+//             <input
+//               type="text"
+//               className="h-full px-8 rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white"
+//               placeholder="Search transcripts..."
+//             />
+//             <button className="h-full px-8 rounded-r-lg p-4 border-t mr-0 border-b border-r bg-blue-500 text-white fond-bold border-blue-500 hover:bg-blue-700">
+//               <AiOutlineSearch />
+//             </button>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AllTranscripts;
+
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { AdminSidebar } from "../../components/admin-sidebar";
 
 const AllTranscripts = () => {
   return (
-    <div className="min-h-screen flex felx-col justify-center relative">
+    <div className="min-h-screen flex felx-col justify-center relative bg-gray-300">
       <div className="flex h-screen"><AdminSidebar/></div>
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between">
-          <h1 className="flex justify-center text-3xl font-bold text-gray">
+          <h1 className="flex justify-center text-3xl text-black">
             All Transcripts
           </h1>
         </div>
         <div className="mt-6">
-          <form className="flex items-center h-[60px] bg-red-500 flex-row">
+          <form className="flex items-center h-[40px] w-500 flex-row">
             <input
               type="text"
-              className="h-full px-8 rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white"
+              className="h-full flex-1 px-4 rounded-l-lg p-2 border-t border-b border-l text-gray-800 border-gray-200 bg-white"
               placeholder="Search transcripts..."
             />
-            <button className="h-full px-8 rounded-r-lg p-4 border-t mr-0 border-b border-r bg-blue-500 text-white fond-bold border-blue-500 hover:bg-blue-700">
+            <button className="h-full px-6 rounded-r-lg p-2 border-t border-b border-r bg-teal-500 text-white fond-bold border-teal-500 hover:bg-teal-700">
               <AiOutlineSearch />
             </button>
           </form>
--- src/pages/adminpg/uploadTrans.jsx
+++ src/pages/adminpg/uploadTrans.jsx
@@ -1,5 +1,101 @@
-import React from "react";
-import { useState } from "react";
+// import React, { useState } from "react";
+// import axios from "axios";
+// import { AdminSidebar } from "../../components/admin-sidebar";
+
+// function UploadTrans() {
+//   const [file, setFile] = useState(null);
+
+//   const handleFileChange = (e) => {
+//     const selectedFile = e.target.files[0];
+//     if (selectedFile && selectedFile.type === "application/pdf") {
+//       setFile(selectedFile);
+//     } else {
+//       setFile(null);
+//     }
+//   };
+
+//   const handleDrop = (e) => {
+//     e.preventDefault();
+//     const selectedFile = e.dataTransfer.files[0];
+//     if (selectedFile && selectedFile.type === "application/pdf") {
+//       setFile(selectedFile);
+//     } else {
+//       setFile(null);
+//     }
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Code to handle file upload here
+//     const formData = new FormData();
+//     formData.append("file", file);
+
+//     const url =
+//       "https://firebasestorage.googleapis.com/v0/b/t-scripty.appspot.com/o/CEC.pdf?alt=media&token=af0d74a4-9b19-4034-97c1-e675e193d02b";
+//     axios
+//       .get("http://localhost:5000/sign-pdf", {
+//         params: {
+//           pdfUrl: url,
+//         },
+//       })
+//       .then((response) => {
+//         console.log(response);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   };
+
+//   return (
+//     <div className="flex h-screen">
+//       <AdminSidebar />
+
+//       <form
+//         onSubmit={handleSubmit}
+//         onDrop={handleDrop}
+//         onDragOver={(e) => e.preventDefault()}
+//       >
+//         <div className="mb-4">
+//           <label className="block text-gray-700 font-bold mb-2" htmlFor="file">
+//             Drag and drop a PDF file here or click to choose a file:
+//           </label>
+//           <input
+//             type="file"
+//             id="file"
+//             className="hidden"
+//             onChange={handleFileChange}
+//             accept=".pdf"
+//           />
+//           <div
+//             className={`w-full px-3 py-32 border border-dashed border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 ${
+//               file ? "bg-gray-100" : ""
+//             }`}
+//             onClick={() => document.getElementById("file").click()}
+//           >
+//             {file ? (
+//               <p className="text-gray-500">File selected: {file.name}</p>
+//             ) : (
+//               <p className="text-gray-500">
+//                 Only PDF files are allowed. Maximum file size: 10 MB.
+//               </p>
+//             )}
+//           </div>
+//         </div>
+//         <button
+//           type="submit"
+//           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+//           disabled={!file}
+//         >
+//           Upload
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default UploadTrans;
+
+import React, { useState } from "react";
 import axios from "axios";
 import { AdminSidebar } from "../../components/admin-sidebar";
 
@@ -7,13 +103,27 @@ function UploadTrans() {
   const [file, setFile] = useState(null);
 
   const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    setFile(file);
+    const selectedFile = e.target.files[0];
+    if (selectedFile && selectedFile.type === "application/pdf") {
+      setFile(selectedFile);
+    } else {
+      setFile(null);
+    }
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const selectedFile = e.dataTransfer.files[0];
+    if (selectedFile && selectedFile.type === "application/pdf") {
+      setFile(selectedFile);
+    } else {
+      setFile(null);
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //Code to handle file upload here
+    // Code to handle file upload here
     const formData = new FormData();
     formData.append("file", file);
 
@@ -33,47 +143,54 @@ function UploadTrans() {
       });
   };
 
-  //   function handleGetHash() {
-  //     const url =
-  //       "https://firebasestorage.googleapis.com/v0/b/t-scripty.appspot.com/o/files%2Fteola-file.pdf?alt=media&token=5e5f52f6-271e-4cb3-96b1-07c041b026eb";
-  //     axios
-  //       .get("http://localhost:5000/get-hash", {
-  //         params: {
-  //           pdfUrl: url,
-  //         },
-  //       })
-  //       .then((response) => {
-  //         console.log(response);
-  //       })
-  //       .catch((error) => {
-  //         console.error(error);
-  //       });
-  //   }
-
   return (
     <div className="flex h-screen">
       <AdminSidebar />
 
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="file">
-            Choose a pdf to upload:
-          </label>
-          <input
-            type="file"
-            id="file"
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-            onChange={handleFileChange}
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-          disabled={!file}
+      <div className="flex-1 flex flex-col justify-center items-center bg-gray-300">
+        <form
+          onSubmit={handleSubmit}
+          onDrop={handleDrop}
+          onDragOver={(e) => e.preventDefault()}
         >
-          Upload
-        </button>
-      </form>
+          <div className="mb-4 text-center">
+            <label
+              className="block text-gray-800 font-bold mb-2"
+              htmlFor="file"
+            >
+              Drag and drop a TRANSCRIPT here or click to choose a file:
+            </label>
+            <input
+              type="file"
+              id="file"
+              className="hidden"
+              onChange={handleFileChange}
+              accept=".pdf"
+            />
+            <div
+              className={`w-full px-3 py-32 border border-dashed border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 ${
+                file ? "bg-gray-100" : ""
+              }`}
+              onClick={() => document.getElementById("file").click()}
+            >
+              {file ? (
+                <p className="text-gray-500">File selected: {file.name}</p>
+              ) : (
+                <p className="text-gray-500">
+                  Only PDF files are allowed. Maximum file size: 10 MB.
+                </p>
+              )}
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-teal-500 text-white px-4 py-2 rounded-md hover:bg-teal-600 focus:outline-none focus:bg-teal-600"
+            disabled={!file}
+          >
+            Upload
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
